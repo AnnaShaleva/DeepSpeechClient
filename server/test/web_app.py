75a591ff267a31700ea993ef6a35758da0e7c53f
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-03 15:30:38 +0300

    Added test web-app with docker

---
+++ server/test/web_app.py
@@ -0,0 +1,12 @@
+app = Flask(__name__)
+
+@app.route('/test', methods=['Get'])
+def apitest():
+    return('Test method')
+
+@app.route('/predict', methods=['Post'])
+def apicall():
+    
+    return 'text'
+
+
